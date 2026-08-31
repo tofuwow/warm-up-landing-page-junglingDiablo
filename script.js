872dc9242c10f80463fc2436c78a4d9e88bd9ecf
anonymouspython96 <emilianmail96@gmail.com>
2026-08-31 16:06:15 +0200

    TODO setted

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const show = document.getElementById("show")
 const hide = document.getElementById("hide")
 const sideMenu = document.getElementById("side-menu")
+// TODO: hiding the side-menu when clicking a button 
 
 show.addEventListener("click", ()=> {
     sideMenu.style.display = "block"

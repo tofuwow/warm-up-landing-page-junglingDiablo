cc9c00d138b15472c96ccebd557ed584569ad2f7
anonymouspython96 <emilianmail96@gmail.com>
2026-08-31 09:25:45 +0200

    I'm stacked without AI but I will not give up!

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// This algorithm it is for make uppear the nav
+let anchorShowMenu = document.getElementById("show")
+const show = document.getElementById("side-menu")
+
+anchorShowMenu.addEventListener("click", function() {
+    alert("You clicked")
+})
+ 
+
 // This algorithm will pick the nav and show the nav if 
 // the nav riches a certain width
 // or when the hamburger is clicked

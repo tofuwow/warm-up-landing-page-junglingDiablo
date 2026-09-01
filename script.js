7cc04408a10cc3f79dc9be564d549d10904c0823
anonymouspython96 <emilianmail96@gmail.com>
2026-09-01 09:13:13 +0200

    TODO done, created a for loop with "of" to be able to close the sideMenu of the website

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const show = document.getElementById("show")
 const hide = document.getElementById("hide")
 const sideMenu = document.getElementById("side-menu")
-// TODO: hiding the side-menu when clicking a button 
+const hideFeauture = document.querySelectorAll(".hide-f")
 
 show.addEventListener("click", ()=> {
     sideMenu.style.display = "block"
@@ -10,3 +10,9 @@ show.addEventListener("click", ()=> {
 hide.addEventListener("click", ()=> {
     sideMenu.style.display = "none"
 })
+
+for(const el of hideFeauture) {
+    el.addEventListener("click", ()=> {
+        sideMenu.style.display = "none"
+    })
+}
